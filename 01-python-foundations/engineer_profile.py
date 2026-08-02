aa4795f5ce82854837766b9161493f64ded936c5
rameshkamakoti <rameshkamakoti@gmail.com>
2026-08-01 19:16:44 -0500

    included profile file

---
+++ 01-python-foundations/engineer_profile.py
@@ -0,0 +1,23 @@
+Name = "Ramesh Kamakoti"
+Age = 46
+Place = "Austin"
+role = "Software Engineer"
+company = "Ramesh Lab"
+experience = 20
+cloud_platform = "AWS"
+containerization = "Kubernetes"
+
+print("=======================================")
+print("Platform Engineer profile")
+print("=======================================")
+
+print(f"Name: {Name}")
+print(f"Age: {Age}")
+print(f"Place: {Place}")
+print(f"Role: {role}")
+print(f"Company: {company}")
+print(f"Experience: {experience} years")
+print(f"Cloud Platform: {cloud_platform}")
+print(f"Containerization: {containerization}")
+
+print("Profile loaded successfully")
