b71e98fe0afac8fd27251573c6b1e22b6d49a725
rameshkamakoti <rameshkamakoti@gmail.com>
2026-08-10 07:07:12 -0500

    included pythin-utils directory

---
+++ python-utils/test.py
@@ -0,0 +1,9 @@
+﻿def has_duplicate(nums):
+    return len(nums) != len(set(nums))
+
+
+numbers1 = [1, 2, 3, 4, 5]
+numbers2 = [1, 2, 2, 4, 5]
+
+print(has_duplicate(numbers1))
+print(has_duplicate(numbers2))
